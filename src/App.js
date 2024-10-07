0e2034bcddc5351e051d6168112fb989c9a1e04e
SUCHIRATHOD <sulochanarathod58@gmail.com>
2024-10-07 17:22:20 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import NavScrollExample from './components/Navbar'
+import Bestsellers from './components/Bestsellers'
+import './styles/Navbar.css'
+import Categories from './components/Categories'
+import Customers from './components/Customers'
+import Paragraph from './components/Paragraph'
+import Footer from './components/Footer'
+import Serve from './components/Serve'
+import Banner from './components/Banner'
+import Searches from './components/Searches'
+import Rights from './components/Rights'
 
 const App = () => {
   return (
-    <div>Hello</div>
+    <div>
+      <NavScrollExample/>
+      <Banner/>
+      <Categories/>
+      <Bestsellers/>
+      
+      <Customers/>
+      <Paragraph/>
+      <Footer/>
+      <Serve/>
+      <Searches/>
+     <Rights/>
+    </div>
   )
 }
 
